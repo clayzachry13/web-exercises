bbf0f98b1a538c56910df5a1263312f53843cd32
Clay Zachry <clay.zachry13@gmail.com>
2021-05-18 21:43:07 -0500

    added a prompt that was followed by a alert

--- js/external.js
+++ js/external.js
@@ -1,3 +1,7 @@
 console.log("Hello from external javaScript");
 
 alert("This is not a scam!");
+
+var userInput = prompt("what is your favorite color?");
+
+alert(userInput + " that is my favorite color too!");
