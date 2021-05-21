b9e4a767db43931c6077304bfb3a8969a4438cd1
Clay Zachry <clay.zachry13@gmail.com>
2021-05-21 18:08:00 -0500

    added notation

--- js/ifElseSwitch.js
+++ js/ifElseSwitch.js
@@ -1,5 +1,9 @@
+
+// javascript function that searches for a matching result to alert the user
+// with a prepared response through if and else statements.
 function colorChecker(x) {
     if (x == "blue"){
+    
         alert("blue is the color of the sky");
 
     }
@@ -14,8 +18,13 @@ function colorChecker(x) {
     }
 }
 
+//Here I alert the user for a respone
 var color = prompt("what color do you like");
 
+//Then I pass that response into the function i made on line 4-18
 colorChecker(color);
 
-
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
+console.log(colors);
+console.log(randomColor);
