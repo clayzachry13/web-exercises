82e20cd41312b3510f7dd4fad900abd8cf6695b6
Clay Zachry <clay.zachry13@gmail.com>
2021-05-24 08:30:59 -0500

    Added new exercises to ifelseswitch js file

--- js/ifElseSwitch.js
+++ js/ifElseSwitch.js
@@ -23,8 +23,44 @@ var color = prompt("what color do you like");
 
 //Then I pass that response into the function i made on line 4-18
 colorChecker(color);
-
+// var colors makes an array of strings
+//var randomColor randomize the colors variable
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var randomColor = colors[Math.floor(Math.random() * colors.length)];
 console.log(colors);
 console.log(randomColor);
+
+colorChecker(randomColor);
+
+var userinput = confirm("would you like to enter a number");
+
+if(userinput === true){
+      var userNumber =  prompt("pick a number.");
+    } 
+    else if(userinput === false){
+      alert("have a great day!");
+    };
+
+
+
+
+
+
+//if(userNumber ==  )
+
+
+var numbers = [1 , 2  , 3 , 4 , 5 ];
+var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+var bill = prompt("what is your bill total?");
+function discount(x,bill) {
+    x * 100 / 100 - bill
+    
+}
+
+console.log(discount);
+
+discount(15)
+
+.console.log(discount);
+
+var billTotal = discount;
