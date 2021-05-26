a560c28cbbd92828d28a0a82c4243c0e55d44efc
Clay Zachry <clay.zachry13@gmail.com>
2021-05-26 15:42:45 -0500

    built a loop to check for odd and even numbers

--- js/loops.js
+++ js/loops.js
@@ -14,3 +14,22 @@ function MuliplicationTable(k) {
 console.log(MuliplicationTable(10));
 console.log(MuliplicationTable(3));
 
+// built a for loop to generate 10 random numbers between 20 and 200
+//check outputs if they  are even or odd.
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+for (var i = 0; i < 10; i++){
+    var random = getRandomInt(20, 200)
+    if (random % 2 === 0) {
+        console.log(" even " + random);
+        
+    }else{
+        console.log(" odd " + random);
+    }
+}
+
