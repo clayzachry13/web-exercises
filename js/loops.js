91ecb80b2cb30ec5c3e46ac447eed73d06cb9811
Clay Zachry <clay.zachry13@gmail.com>
2021-05-27 12:16:02 -0500

    reworking exercieses and refining them

--- js/loops.js
+++ js/loops.js
@@ -3,53 +3,27 @@
 // Created a function that takes a number and console.logs
 //the multiplication table for  1 through 10
 
-function showMuliplicationTable(x) {
-    for(var i = 1; i < 10; i++){
-        console.log(x + "X" + i + "=" + (x * i));
-    }
+function MultiTable(x) {
+
+    for(var i = 1; i <= 10; i++)
+        console.log(x + "X" + i + "=" + (i * x));
     
 }
 
-showMuliplicationTable(2)
+MultiTable(3)
 
 // built a for loop to generate 10 random numbers between 20 and 200
 //check outputs if they  are even or odd.
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
 
-for(var b = 1; b <= 10; b++){
-   var randomNumber = getRandomInt(20, 200);
-    if (randomNumber % 2 === 0) {
-        console.log(randomNumber + "even");
-        
-    }else{
-        console.log(randomNumber + " odd");
-    }
-}
 
 
 // created a loop that consol.logs a number like 1 then logs 1
 // if number is two it logs 22, if number 3 it logs 333 ect
 
-for(var k = 1; k < 10; k++){
-    var output = "";
-    for(var z = 1; z <= k; z++){
-     output = output + k;
-     
-    }
-    console.log(output);
-}
 
 
 
 
 // built a function that removes 5 from the starting number
 // using a console log in a loop
-
-for(var p = 100; p >= 5; p=p-5){
-    console.log(p);
-}
