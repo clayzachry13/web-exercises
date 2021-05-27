61c7b2717482a5b273ded3078bb24b8ba4fd6049
Clay Zachry <clay.zachry13@gmail.com>
2021-05-26 20:40:52 -0500

    built a function and put a for loop inside that function to accpect a number which then shows the muliplication table for

--- js/loops.js
+++ js/loops.js
@@ -3,33 +3,30 @@
 // Created a function that takes a number and console.logs
 //the multiplication table for  1 through 10
 
-function MuliplicationTable(k) {
-
-    for (var i = 0; i < 10; i++) {
-        console.log(k + "*" + i + "=" + (i * k));
+function showMuliplicationTable(x) {
+    for(var i = 1; i < 10; i++){
+        console.log(x + "X" + i + "=" + (x * i));
     }
     
 }
 
-console.log(MuliplicationTable(10));
-console.log(MuliplicationTable(3));
+showMuliplicationTable(2)
 
 // built a for loop to generate 10 random numbers between 20 and 200
 //check outputs if they  are even or odd.
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
 
-for (var i = 0; i < 10; i++){
-    var random = getRandomInt(20, 200)
-    if (random % 2 === 0) {
-        console.log(" even " + random);
-        
-    }else{
-        console.log(" odd " + random);
-    }
-}
+
+
+
+// created a loop that consol.logs a number like 1 then logs 1
+// if number is two it logs 22, if number 3 it logs 333 ect
+
+
+
+
+
+// built a function that removes 5 from the starting number
+// using a console log in a loop
+
 
