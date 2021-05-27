fe1629bf13d2a57f18f6d91932d2f5fbcf6913fd
Clay Zachry <clay.zachry13@gmail.com>
2021-05-26 20:51:55 -0500

    built a odd and even checker using a random number gen inside a loop

--- js/loops.js
+++ js/loops.js
@@ -15,8 +15,21 @@ showMuliplicationTable(2)
 // built a for loop to generate 10 random numbers between 20 and 200
 //check outputs if they  are even or odd.
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
-
+for(var b = 1; b <= 10; b++){
+   var randomNumber = getRandomInt(20, 200);
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + "even");
+        
+    }else{
+        console.log(randomNumber + " odd");
+    }
+}
 
 
 // created a loop that consol.logs a number like 1 then logs 1
