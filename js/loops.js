56172da9c79bcf602b0d7924bffd79f7f0855974
Clay Zachry <clay.zachry13@gmail.com>
2021-05-27 12:48:59 -0500

    reworking exercieses

--- js/loops.js
+++ js/loops.js
@@ -1,11 +1,11 @@
 "use strict";
 
-// Created a function that takes a number and console.logs
+// Created a function that takes a number and console logs
 //the multiplication table for  1 through 10
 
 function MultiTable(x) {
 
-    for(var i = 1; i <= 10; i++)
+    for(var i = 1; i <= 10; i++);
         console.log(x + "X" + i + "=" + (i * x));
     
 }
@@ -15,15 +15,25 @@ MultiTable(3)
 // built a for loop to generate 10 random numbers between 20 and 200
 //check outputs if they  are even or odd.
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
 
 
+
+for(let i = 0; i <= 10; i++){
+    var number = getRandomInt(20, 200);
+    
+    if (number % 2 === 0) {
+        console.log(number + "even");
+        
+    }else{
+        console.log(number + "odd");
+    }
+    
+}
 
 // created a loop that consol.logs a number like 1 then logs 1
-// if number is two it logs 22, if number 3 it logs 333 ect
-
-
-
-
-
-// built a function that removes 5 from the starting number
-// using a console log in a loop
+// if number is two it logs 22, if number 3 it logs 333  
